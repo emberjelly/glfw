ce7cf3a96e4742ffbddd48b116f94f048860397d
Chip <chip@claynoik.com>
2013-07-07 14:17:23 +0100

    Initialise OpenAL as well.
    This has introduced a "defaultlib conflicts with use of other libs"
    linker warning... probably need to recompile OpenAL Soft from source to
    solve this properly.

--- glfw_1/inc/Main.h
+++ glfw_1/inc/Main.h
@@ -3,3 +3,5 @@
 #endif // WINDOWS
 
 #include <GLFW/glfw3.h>
+#include <AL/al.h>
+#include <AL/alc.h>
--- glfw_1/src/Main.cpp
+++ glfw_1/src/Main.cpp
@@ -6,6 +6,7 @@ int main()
 	FreeConsole();
 #endif // WINDOWS
 
+	// Graphics setup
 	if (!glfwInit())
 		return -1;
 
@@ -18,12 +19,46 @@ int main()
 
 	glfwMakeContextCurrent(window);
 
+	// Audio setup
+	ALCdevice *audioDevice = alcOpenDevice(NULL);
+	if (!audioDevice)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	ALCcontext *audioContext = alcCreateContext(audioDevice, NULL);
+	if (!audioContext)
+	{
+		alcCloseDevice(audioDevice);
+		glfwTerminate();
+		return -1;
+	}
+
+	if (!alcMakeContextCurrent(audioContext))
+	{
+		alcMakeContextCurrent(NULL);
+		alcDestroyContext(audioContext);
+		alcCloseDevice(audioDevice);
+	}
+
+
+	// Main loop
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+
+	// Audio cleanup
+	alcMakeContextCurrent(NULL);
+	alcDestroyContext(audioContext);
+	alcCloseDevice(audioDevice);
+
+	// Graphics cleanup
 	glfwTerminate();
+
+	// Done
 	return 0;
 }
